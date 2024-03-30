e0fc1857603f078d1246dd5bdd2f2ae09c601b15
kanta13jp1 <kanta13jp@gmail.com>
2024-03-30 14:46:28 +0900

    予約フォームに日付入力を追加

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -27,9 +27,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "d9848113a6289f59689c83cf0b90e58c",
-"/": "d9848113a6289f59689c83cf0b90e58c",
-"main.dart.js": "7a174cb27aef165e94a6ff8a9dd2fe98",
+"index.html": "e1a19a6b5ea1a88cc03b3c35a74443e9",
+"/": "e1a19a6b5ea1a88cc03b3c35a74443e9",
+"main.dart.js": "54cd4a9af06cf0db17c9cbfad22034d9",
 "manifest.json": "fa2087bf001a6a093a6b84959519af78",
 "version.json": "8b390d481aa8c32b04a89f1288da1b9c"};
 // The application shell files that are downloaded before a service worker can
--- lib/booking_form.dart
+++ lib/booking_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class BookingForm extends StatefulWidget {
   @override
@@ -9,13 +10,37 @@ class _BookingFormState extends State<BookingForm> {
   final _formKey = GlobalKey<FormState>();
   String _name = '';
   String _email = '';
+  DateTime _selectedDate = DateTime.now(); // 日付選択のための状態変数
 
   void _submitForm() {
-    if (_formKey.currentState!.validate()) {
+    final form = _formKey.currentState;
+    if (form != null && form.validate()) {
+      form.save(); // ここで onSaved コールバックが呼ばれ、_name と _email に値が保存されます。
+
       // フォームが有効な場合、ここで予約処理を実装できます。
+      // 予約日を含めて送信します。
+      print('名前: $_name');
+      print('メールアドレス: $_email');
+      print('選択された日付: ${DateFormat('yyyy年MM月dd日').format(_selectedDate)}');
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('予約が送信されました')),
+        SnackBar(
+            content: Text(
+                '予約が送信されました: 名前: $_name, メール: $_email, 日付: ${DateFormat('yyyy年MM月dd日').format(_selectedDate)}')),
       );
+    }
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: _selectedDate,
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != _selectedDate) {
+      setState(() {
+        _selectedDate = picked;
+      });
     }
   }
 
@@ -45,6 +70,14 @@ class _BookingFormState extends State<BookingForm> {
               return null;
             },
             onSaved: (value) => _email = value!,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () => _selectDate(context),
+              child: Text(
+                  '日付を選択: ${DateFormat('yyyy年MM月dd日').format(_selectedDate)}'),
+            ),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 16.0),
