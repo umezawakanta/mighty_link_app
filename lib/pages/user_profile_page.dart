9c8caf4efcea93631265532315171b95e9896d48
kanta13jp1 <kanta13jp@gmail.com>
2024-04-27 14:17:19 +0900

    誕生日を追加

--- lib/pages/user_profile_page.dart
+++ lib/pages/user_profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class UserProfilePage extends StatefulWidget {
@@ -97,6 +98,7 @@ class _UserProfilePageState extends State<UserProfilePage> {
             )
           : SizedBox(); // avatar_urlがnullの場合は何も表示しない
     }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('User Profile'),
@@ -130,6 +132,10 @@ class _UserProfilePageState extends State<UserProfilePage> {
                     controller: _emailController,
                     decoration: InputDecoration(labelText: 'Email'),
                   ),
+                  SizedBox(height: 10),
+                  Text(
+                      '誕生日: ${DateFormat('MM/dd/yyyy').format(DateTime.parse(userData!['birthday']))}',
+                      style: TextStyle(fontSize: 20)),
                   SizedBox(height: 10),
                   Text('Age: ${userData!['age']}',
                       style: TextStyle(fontSize: 20)),
